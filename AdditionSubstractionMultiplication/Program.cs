6f767fa01292750651727717307b4fe994d895a4
shubham07h <shubham1609sharma@gmail.com>
2022-11-29 17:05:01 +0530

    [shubham]-addition substraction multiplication

---
+++ AdditionSubstractionMultiplication/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Addition!");
+namespace AddTwoNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2, sum;
+            Console.Write("Enter First Number : ");
+            num1 = int.Parse(Console.ReadLine());
+            Console.Write("Enter Second Number : ");
+            num2 = int.Parse(Console.ReadLine());
+            sum = num1 + num2;
+            Console.Write("Sum of Both Numbers = " +sum);
+            Console.ReadLine();
+        }
+    }
+}
